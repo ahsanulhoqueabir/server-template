bdd29ff8520c399fb1c878ac1d9649855e07ab48
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-01-31 17:22:53 +0600

    vercel configure file updated

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
