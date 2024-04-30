018430f0bb434d32620b271bf6a738c48b733b6a
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-04-30 23:31:51 +0600

    Updated

--- index.js
+++ index.js
@@ -1,51 +1,97 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3000;
-
-// middlewire for data parsing and cors
-app.use(cors());
+const app = express();
+const port = process.env.port || 3000;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ahsanulhoqueabir.netlify.app",
+      //add your domain here for production use
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// mongodb connection 
-const uri = "mongodb://0.0.0.0:27017/";
-// "mongodb+srv://user:password@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require("mongodb");
+
+// mongodb compass connection
+// const uri = "mongodb://0.0.0.0:27017/";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASWORD}@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 /*  Mongodb atlas connection*/
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 /*  Mongodb compass connection*/
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const dbname = client.db("dbname");
-    const collectionName = allInformation.collection("collectionName");
+    const dbName = client.db("dbName");
+    const users = dbName.collection("users");
+    const collection1 = dbName.collection("collection1");
 
-   
-   
+    // --------------- Custom Middleware -----------------
+    const verifyJWT = (req, res, next) => {
+      const authorized = req.headers.authorization;
+      if (!authorized) {
+        return res
+          .status(401)
+          .json({ error: true, message: "Authentication Error" });
+      }
+      const token = authorized.split(" ")[1];
+      jwt.verify(
+        token,
+        // process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+          if (err) {
+            return res
+              .status(403)
+              .json({ error: true, message: "Token is not valid" });
+          }
+          req.decoded = decoded;
+          next();
+        }
+      );
+    };
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // use verifyJWT before calling this
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const user = await users.findOne({ email: email });
+
+      if (user.role === "admin") {
+        next();
+      } else {
+        res.status(403).json({ error: true, message: "Unauthorized  User" });
+      }
+    };
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "30d",
+      });
+      res.send({ token });
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.log);
---
+++ readme.md
@@ -0,0 +1,21 @@
+## npm install command line
+
+```
+
+npm i cors mongodb express jsonwebtoken dotenv
+
+```
+
+## Include to .gitignore file
+
+```
+.env
+node_modules/
+npm-debug*
+.log*
+.vercel
+
+```
+
+
+
